f22775107696d164bad4171d6d7f272f7e23a936
stefandebruyn <stefandebruyn@utexas.edu>
2019-07-08 20:37:38 -0500

    Add basic Euler bound integration with Cd scheduling

--- src/curve_generation.cpp
+++ src/curve_generation.cpp
@@ -1,9 +1,56 @@
 #include "curve_generation.hpp"
 
-namespace cgen {
+namespace aimbot {
 
-void set_extrapolation_timestep(float k_timestep) {
-  extrapolation_timestep = k_timestep;
-}
+  namespace cgen {
 
-}; // end namespace cgen
+    void set_timestep(float k_timestep) {
+      timestep = k_timestep;
+    }
+
+    void set_rocket_cd_schedule(CdSchedule* k_sched) {
+      rocket_cd_sched = k_sched;
+    }
+
+    void set_airbrake_cd_schedule(CdSchedule* k_sched) {
+      airbrake_cd_sched = k_sched;
+    }
+
+    bounds_t integrate_euler(const rocket_t& k_rocket,
+                             const state_t& k_state)
+    {
+      bounds_t bounds;
+      rocket_t rocket_extrap = k_rocket;
+      state_t state_extrap = k_state;
+
+      // Upper bound, airbrakes retracted
+      while (state_extrap.velocity > 0) {
+        float cd = rocket_cd_sched->get_cd(state_extrap.velocity);
+        rocket_extrap.drag_coefficient = cd;
+        float a = simple_net_acceleration(rocket_extrap, state_extrap);
+        state_extrap.velocity += a * timestep;
+        state_extrap.altitude += state_extrap.velocity * timestep;
+      }
+
+      bounds.upper = state_extrap.altitude;
+      rocket_extrap = k_rocket;
+      state_extrap = k_state;
+
+      // Lower bound, airbrakes extended
+      while (state_extrap.velocity > 0) {
+        float cd = rocket_cd_sched->get_cd(state_extrap.velocity);
+        rocket_extrap.drag_coefficient =
+            cd + airbrake_cd_sched->get_cd(state_extrap.velocity);
+        float a = simple_net_acceleration(rocket_extrap, state_extrap);
+        state_extrap.velocity += a * timestep;
+        state_extrap.altitude += state_extrap.velocity * timestep;
+      }
+
+      bounds.lower = state_extrap.altitude;
+
+      return bounds;
+    }
+
+  }; // end namespace cgen
+
+}; // end namespace aimbot
--- src/curve_generation.hpp
+++ src/curve_generation.hpp
@@ -1,16 +1,33 @@
 #ifndef AIMBOT_CURVE_GENERATION_HPP
 #define AIMBOT_CURVE_GENERATION_HPP
 
-namespace cgen {
+#include "cd_schedule.hpp"
+#include "drag_force.hpp"
+#include "standard_atmosphere.hpp"
 
-typedef struct AltitudeBounds {
-  float lower;
-  float upper;
-} bounds_t;
+namespace aimbot {
 
-static float extrapolation_timestep = 0.01;
+  namespace cgen {
 
-void set_extrapolation_timestep(float k_timestep);
+    typedef struct AltitudeBounds {
+      float lower;
+      float upper;
+    } bounds_t;
+
+    static float timestep = 0.01;
+
+    static CdSchedule* rocket_cd_sched;
+    static CdSchedule* airbrake_cd_sched;
+
+    void set_timestep(float k_timestep);
+
+    void set_rocket_cd_schedule(CdSchedule* k_sched);
+
+    void set_airbrake_cd_schedule(CdSchedule* k_sched);
+
+    bounds_t integrate_euler(const rocket_t& k_rocket, const state_t& k_state);
+
+  }; // end namespace cgen
 
 }; // end namespace cgen
 
